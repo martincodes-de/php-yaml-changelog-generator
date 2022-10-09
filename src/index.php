5b1bd83b6d59d122d4419fd8680495482bc36d86
martincodes <62392843+martincodes-de@users.noreply.github.com>
2022-10-09 17:44:15 +0200

    cleanup

--- src/index.php
+++ src/index.php
@@ -6,22 +6,6 @@ $changelogDirectoryPath = __DIR__."/../changelog";
 $changelogDirectory = scandir($changelogDirectoryPath);
 $excludedFiles = [".", "..", "template.yaml", "releaseinfo.yaml"];
 
-$files = array_filter($changelogDirectory, fn ($file) => !in_array($file, $excludedFiles));
-
-$changelog = [];
-
-foreach ($files as $fileName) {
-    $filePath = $changelogDirectoryPath."/{$fileName}";
-    $isDir = is_dir($filePath);
-
-    if (!$isDir) {
-        $entry = yaml_parse_file($filePath);
-        $addedAtDateTime = date(DATE_ATOM, filemtime($filePath));
-        $entry["added_at"] = $addedDateTime;
-        $changelog[] = $entry;
-    }
-}
-
 $cc = new ChangelogCreator($changelogDirectoryPath, $excludedFiles);
 
-var_dump($cc);
+var_dump($cc->getChangelog());
